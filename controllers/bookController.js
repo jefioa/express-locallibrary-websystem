2d3f764617214f55863d48b3c0af57deb6551005
jefioa <cte117jau@icloud.com>
2026-05-28 07:16:53 +0000

    ホームページタイトル変更

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -22,7 +22,7 @@ exports.index = async (req, res, next) => {
   ]);
 
   res.render("index", {
-    title: "Local Library Home",
+    title: "地域図書館ホームページ",
     book_count: numBooks,
     book_instance_count: numBookInstances,
     book_instance_available_count: numAvailableBookInstances,
